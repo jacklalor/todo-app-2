c12b5af72b17d249e335900f7ea6df10e6377d8e
jack <jack.lalor0@gmail.com>
2023-11-13 12:03:31 +0000

    Added user id todos

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Todo;
+
 
 class User extends Authenticatable
 {
@@ -42,4 +44,12 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    /**
+     * Get the user that owns the phone.
+     */
+    public function todos() 
+    {
+        return $this->hasMany(Todo::class);
+    }
 }
--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -5,6 +5,9 @@
 
 <p>{{ $todo->title }}</p>
 <p>{{ $todo->body }}</p>
+<p>{{ $todo->user->name }}</p>
+
+
 
 <div>
     <a href="{{ route('todos.edit', $todo->id) }}">Edit</a>
